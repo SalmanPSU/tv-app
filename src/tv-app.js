4078937ead613b96adbdf7c09e8e11ae11edcdc1
salmanalmutairi <salman@psu.edu>
2023-11-14 13:45:00 -0500

    v2

--- src/tv-app.js
+++ src/tv-app.js
@@ -11,6 +11,11 @@ export class TvApp extends LitElement {
     this.name = '';
     this.source = new URL('../assets/channels.json', import.meta.url).href;
     this.listings = [];
+    this.activeItem = {
+      title: null,
+      id: null,
+      description: null,
+    };
   }
   // convention I enjoy using to define the tag's name
   static get tag() {
@@ -79,16 +84,58 @@ export class TvApp extends LitElement {
         )
       }
       </div>
-      <div>
+      <div style= "display: inline-flex;">
+      ${this.activeItem.name}
         <!-- video -->
+        <div>
+        <iframe
+          width="750"
+          height="400"
+          src="${this.createSource()}" 
+          frameborder="0"
+          allowfullscreen
+        ></iframe>
+        </div>
         <!-- discord / chat - optional -->
+        <div style= "margin-right: 300;">
+        <iframe
+          src="https://discord.com/widget?id=YOUR_DISCORD_SERVER_ID&theme=dark"
+          width="400"
+          height="800"
+          display: inline-flex;
+          allowtransparency="true"
+          frameborder="0"
+          (attribute)sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+        ></iframe>
+        </div>
       </div>
       <!-- dialog -->
       <sl-dialog label="Dialog" class="dialog">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      ${this.activeItem.title},
+      ${this.activeItem.description}
         <sl-button slot="footer" variant="primary" @click="${this.closeDialog}">Close</sl-button>
       </sl-dialog>
     `;
+  }
+
+  changeVideo() {
+    const iframe = this.shadowRoot.querySelector('iframe');
+    iframe.src = this.createSource();
+  }
+
+   extractVideoId(link) {
+    try {
+      const url = new URL(link);
+      const searchParams = new URLSearchParams(url.search);
+      return searchParams.get("v");
+    } catch (error) {
+      console.error("Invalid URL:", link);
+      return null;
+    }
+  }
+  
+  createSource() {
+    return "https://www.youtube.com/embed/" + this.extractVideoId(this.activeItem.video);
   }
 
   closeDialog(e) {
