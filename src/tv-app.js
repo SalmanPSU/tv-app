a88d8c62487f8cc444307a54f334dc9b70faa986
salmanalmutairi <salman@psu.edu>
2023-12-14 01:19:08 +0300

    Update tv-app.js

--- src/tv-app.js
+++ src/tv-app.js
@@ -2,6 +2,7 @@
 import { LitElement, html, css } from 'lit';
 import '@shoelace-style/shoelace/dist/components/dialog/dialog.js';
 import '@shoelace-style/shoelace/dist/components/button/button.js';
+import "@lrnwebcomponents/video-player/video-player.js";
 import "./tv-channel.js";
 
 export class TvApp extends LitElement {
@@ -11,11 +12,22 @@ export class TvApp extends LitElement {
     this.name = '';
     this.source = new URL('../assets/channels.json', import.meta.url).href;
     this.listings = [];
-    this.activeItem = {
-      title: null,
+    this.temporaryItem = {
       id: null,
+      title: null,
+      presenter: null,
+      time: null,
       description: null,
-    };
+      video: null
+    }
+    this.activeItem = {
+      id: null,
+      title: null,
+      presenter: null,
+      time: null,
+      description: null,
+      video: null
+    }
   }
   // convention I enjoy using to define the tag's name
   static get tag() {
@@ -27,6 +39,8 @@ export class TvApp extends LitElement {
       name: { type: String },
       source: { type: String },
       listings: { type: Array },
+      temporaryItem: {type: Object },
+      activeItem: { type: Object }
     };
   }
   // LitElement convention for applying styles JUST to our element
@@ -35,32 +49,72 @@ export class TvApp extends LitElement {
       css`
       :host {
         display: block;
-        margin: 16px;
-        padding: 16px;
       }
-      .listing-container {
-        justify-self: center;
-        max-width: 1344px;
-        justify-items: left;
+      header {
+        color: #000;
+        padding: 16px;
+        text-align: center;
+      }
+
+      .h1 {
+        font-size: 32px;
+        margin-bottom: 16px;
+      }
+
+      .channel-container {
+        margin-left: 16px;
+        max-width: 96%;
         display: flex;
         flex-direction: row;
         flex-grow: 1;
         flex-wrap: nowrap;
         overflow-x: auto;
         overflow-y: auto;
-        padding-left: .5rem;
-        padding-right: .5rem;
-        text-rendering: optimizeLegibility;
-        width: 100%;
-        margin: 0 auto;
-        position: relative;
-        animation-delay: 1s;
-        animation-duration: 1s;
-        line-height: 1.5;
-        font-size: 1em;
       }
-      h5 {
-        font-weight: 400;
+
+      .player-container {
+        margin: 32px 0 0 16px;
+        border-radius: 8px;
+        display: inline-flex;
+        height: 600px;
+      }
+
+      .player {
+        width: 900px;
+        height: 600px;
+      }
+
+      .discord {
+        margin-left: 32px;
+        width: 450px;
+      }
+
+      .discord widgetbot {
+        display: inline-block;
+        overflow: hidden;
+        background-color: rgb(54, 57, 62);
+        border-radius: 8px;
+        vertical-align: top;
+        width: 100%;
+        height: 100%;
+      }
+
+      .discord iframe {
+        border: none;
+        width: 100%;
+        height: 100%;
+      }
+
+      #description {
+        margin-left: 16px;
+        width: 900px;
+        cursor: default;
+      }
+
+      @media(max-width: 999px) {
+        .discord widgetbot {
+          display: none;
+        }
       }
       .
       `,
@@ -69,17 +123,20 @@ export class TvApp extends LitElement {
   // LitElement rendering template of your element
   render() {
     return html`
-      <h2>${this.name}</h2>
-      <div class="listing-container">
-      ${
-        this.listings.map(
-          (item) => html`
-            <tv-channel 
-              title="${item.title}"
-              presenter="${item.metadata.author}"
-              @click="${this.itemClick}"
-            >
-            </tv-channel>
+    <header><h2>${this.name}</h2></header>
+      <div class="channel-container">
+        ${
+          this.listings.map(
+            (item) => html`
+              <tv-channel 
+                id="${item.id}"
+                title="${item.title}"
+                presenter="${item.metadata.author}"
+                description="${item.description}"
+                video="${item.metadata.source}"
+                time="${item.metadata.time}"
+                @click="${this.openDialog}">
+              </tv-channel>
           `
         )
       }
